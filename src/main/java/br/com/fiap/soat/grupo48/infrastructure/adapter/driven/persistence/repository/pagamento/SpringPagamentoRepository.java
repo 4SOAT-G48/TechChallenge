71fb8eae8c2d8f54dae4c14798c3fd6f17d93a2b
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-01-29 23:26:00 -0300

    Entregável: fake Webhook confirmação de pagamento (seleção randomica de pagamento)

--- src/main/java/br/com/fiap/soat/grupo48/infrastructure/adapter/driven/persistence/repository/pagamento/SpringPagamentoRepository.java
+++ src/main/java/br/com/fiap/soat/grupo48/infrastructure/adapter/driven/persistence/repository/pagamento/SpringPagamentoRepository.java
@@ -1,11 +1,15 @@
 package br.com.fiap.soat.grupo48.infrastructure.adapter.driven.persistence.repository.pagamento;
 
+import br.com.fiap.soat.grupo48.application.pagamento.model.SituacaoPagamento;
 import br.com.fiap.soat.grupo48.infrastructure.adapter.driven.persistence.entity.PagamentoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface SpringPagamentoRepository extends JpaRepository<PagamentoEntity, UUID> {
+
+    List<PagamentoEntity> findBySituacaoPagamento(SituacaoPagamento situacaoPagamento);
 }
