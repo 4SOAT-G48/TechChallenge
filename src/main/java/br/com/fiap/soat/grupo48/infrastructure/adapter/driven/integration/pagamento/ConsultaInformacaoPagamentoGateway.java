7a3834d895ce9b04ad6d14ad390d5edec4371270
André Felipe Laus <andre.felipe.laus@gmail.com>
2024-01-29 23:26:15 -0300

    Entregável: fake Webhook confirmação de pagamento (seleção randomica de pagamento)

--- src/main/java/br/com/fiap/soat/grupo48/infrastructure/adapter/driven/integration/pagamento/ConsultaInformacaoPagamentoGateway.java
+++ src/main/java/br/com/fiap/soat/grupo48/infrastructure/adapter/driven/integration/pagamento/ConsultaInformacaoPagamentoGateway.java
@@ -4,20 +4,32 @@ import br.com.fiap.soat.grupo48.application.pagamento.model.Pagamento;
 import br.com.fiap.soat.grupo48.application.pagamento.model.SituacaoPagamento;
 import br.com.fiap.soat.grupo48.application.pagamento.model.TipoPagamento;
 import br.com.fiap.soat.grupo48.application.pagamento.port.spi.IConsultaInformacaoPagamentoIntegartionGateway;
+import br.com.fiap.soat.grupo48.infrastructure.adapter.driven.persistence.entity.PagamentoEntity;
+import br.com.fiap.soat.grupo48.infrastructure.adapter.driven.persistence.repository.pagamento.SpringPagamentoRepository;
+import br.com.fiap.soat.grupo48.infrastructure.adapter.driven.persistence.repository.pedido.SpringPedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
 @Component
 public class ConsultaInformacaoPagamentoGateway implements IConsultaInformacaoPagamentoIntegartionGateway {
 
+    @Autowired
+    private SpringPagamentoRepository springPagamentoRepository;
+
     @Override
     public Pagamento buscarSituacaoPagamento(TipoPagamento tipoPagamento, String id) {
         Random random = new Random();
         double randomNumber = random.nextDouble();
 
-        UUID codigo_pagamento = UUID.fromString("3855b70e-9e08-4935-833c-3ba7532b4497");
+        //busca todos os pagamentos que estão aguardando pagamento
+        List<PagamentoEntity> bySituacaoPagamento = this.springPagamentoRepository.findBySituacaoPagamento(SituacaoPagamento.AGUARDANDO_PAGAMENTO);
+        //escolhe um randomicamente
+        PagamentoEntity pagamento = bySituacaoPagamento.get(random.nextInt(bySituacaoPagamento.size()));
+        UUID codigo_pagamento = pagamento.getCodigo();
         if (randomNumber <= 0.8) {
             return new Pagamento(codigo_pagamento,null, SituacaoPagamento.APROVADO);
         } else {
